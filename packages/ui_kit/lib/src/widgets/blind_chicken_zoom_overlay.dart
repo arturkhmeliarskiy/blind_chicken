688f5683ba3092f4038cb3de0277099e432b7d21
arturkhmeliarskiy <67556260+arturkhmeliarskiy@users.noreply.github.com>
2024-12-03 23:07:36 +0300

    update info

--- packages/ui_kit/lib/src/widgets/blind_chicken_zoom_overlay.dart
+++ packages/ui_kit/lib/src/widgets/blind_chicken_zoom_overlay.dart
@@ -102,8 +102,6 @@ class _BlindChickenZoomOverlayState extends State<BlindChickenZoomOverlay>
   OverlayEntry? _overlayEntry;
   bool _isZooming = false;
   bool _isPosition = false;
-  Offset _differenceFocalPoint = Offset(0, 0);
-  Offset _translationDelta = Offset(0, 0);
   int _touchCount = 0;
   Matrix4 _transformMatrix = Matrix4.identity();
 
@@ -178,21 +176,21 @@ class _BlindChickenZoomOverlayState extends State<BlindChickenZoomOverlay>
 
   void onScaleUpdate(ScaleUpdateDetails details) {
     if (!_isZooming || _controllerReset.isAnimating) return;
+    Offset translationDelta = Offset(0, 0);
 
     if (details.pointerCount > 1) {
-      _translationDelta = details.focalPoint - _startFocalPoint;
+      translationDelta = details.focalPoint - _startFocalPoint;
       _translate = Matrix4.translation(
-        Vector3(_translationDelta.dx, _translationDelta.dy, 0),
+        Vector3(translationDelta.dx, translationDelta.dy, 0),
       );
     } else {
       if (!_isPosition) {
-        _translationDelta = details.focalPoint + _differenceFocalPoint - _startFocalPoint;
+        translationDelta = details.focalPoint - details.localFocalPoint - _startFocalPoint;
         _translate = Matrix4.translation(
-          Vector3(_translationDelta.dx, _translationDelta.dy, 0),
+          Vector3(translationDelta.dx, translationDelta.dy, 0),
         );
       } else {
         setState(() {
-          _differenceFocalPoint = _translationDelta - details.focalPoint - _startFocalPoint;
           _isPosition = false;
         });
       }
@@ -200,7 +198,7 @@ class _BlindChickenZoomOverlayState extends State<BlindChickenZoomOverlay>
 
     final renderBox = context.findRenderObject() as RenderBox;
     final focalPoint = renderBox.globalToLocal(
-      details.focalPoint - _translationDelta,
+      details.focalPoint - translationDelta,
     );
 
     var scaleby = details.scale;
